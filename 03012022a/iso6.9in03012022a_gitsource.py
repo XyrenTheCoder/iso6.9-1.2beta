e79dbfd76bfd22fd2902472b9d7208efa5c9188f
Kamilla Youver <95626487+archisha69@users.noreply.github.com>
2022-01-04 16:28:31 +0800

    Update iso6.9in03012022a_gitsource.py

--- 03012022a/iso6.9in03012022a_gitsource.py
+++ 03012022a/iso6.9in03012022a_gitsource.py
@@ -13,6 +13,7 @@ from random import randint
 from discord.errors import InvalidArgument
 from discord.ext import commands
 from discord.ext.commands import *
+from keep_alive import keep_alive
 
 intents = discord.Intents(messages=True, members=True, guilds=True)
 
@@ -846,6 +847,7 @@ async def avatar(ctx, username: discord.User=None):
         print(f'[log] {ctx.author} requested ]avatar.')
         
 # start
+keep_alive()
 bot.run(token)
 
 
